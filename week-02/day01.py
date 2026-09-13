6ecce3286b97a153e24d2a318dde1c60495c10ef
CodeByAdi98 <adityababurajan@gmail.com>
2026-09-13 18:24:48 +0200

    Week 2 day 1: joint dict and loop

---
+++ week-02/day01.py
@@ -0,0 +1,6 @@
+joints = {"shoulder":-40.00, "elbow":32.30, "wrist":-15.01, "hip":10.23, "knee":-25.02, "ankle":1.23}
+arm_joints = {"shoulder", "elbow", "wrist"}
+position = (1.23, -2.54, 1.26)
+
+for joint in joints:
+    print(joint, joints[joint])
